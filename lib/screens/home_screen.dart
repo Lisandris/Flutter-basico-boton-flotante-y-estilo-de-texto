4b073ef7ba2905c08d424699fb640c8b43f0d5d5
Lisandris <lis170694@gmail.com>
2022-07-06 23:33:42 +0200

    first commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+
+class HomeScreen extends StatelessWidget {
+
+  const HomeScreen({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    const fontSize30 = TextStyle (fontSize: 30 );
+   
+    return Scaffold(
+      appBar: AppBar( 
+        title: const Text (' Hola Lis'),
+        elevation: 0,
+      ),
+      body:  Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text('Numero de Clicks', style: fontSize30 ),
+              Text('10', style: fontSize30 ),
+             ], 
+             ),
+      ),
+         floatingActionButton: FloatingActionButton(
+          child: const Icon( Icons.ac_unit_sharp),
+            onPressed: () {
+               print( 'hola Lis');
+            },
+        ),
+    );
+   }
+}
